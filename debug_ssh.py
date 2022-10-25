e5efa646a2544ef7c432b4bc603d659c50d9cbd8
Drazen Dodik <drazend@live.fi>
2022-10-25 09:00:54 +0300

    move spark to root

---
+++ debug_ssh.py
@@ -0,0 +1,16 @@
+import debugpy
+import numpy as np
+import time
+ 
+debugpy.listen(5678)
+ 
+# The script is halted here, until a debugger is attached
+debugpy.wait_for_client()
+ 
+for x in range(1,20) :
+    name = "teppo"
+    print(name)
+    print(f"Doing computation {x}")
+    data = np.random.random((50,50))
+    sum = np.sum(data)
+    time.sleep(2)
